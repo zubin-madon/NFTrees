47da947046b624556e4e1123aa0270f6b8a0eaa6
zubin-madon <madon.zubin@gmail.com>
2022-07-10 12:53:52 +0530

    Add Moralis wallet login, Mint function and mint on Rinkeby testnet.

--- src/static/contract.js
+++ src/static/contract.js
@@ -31,10 +31,23 @@ The `piggybankContract` is compiled from:
 */
 
 const forwarderOrigin = 'http://localhost:8000'
+let web3;
+
 
 const initialize = () => {
+  const serverUrl = "https://fzqqgw1gnhcu.usemoralis.com:2053/server";
+  const appId = "wZRtpBnfIAxoDmOL9rWnH6a9QCWPxSgpcm6iuFeA";
+  Moralis.initialize(serverUrl); // Application id from moralis.io
+  Moralis.serverURL = appId; //Server url from moralis.io
+
+  const contractAddress = "0x930061094ec65f7d91199e30DdBd5B0dDdc98Eb6";
+
+
   //Basic Actions Section
   const onboardButton = document.getElementById('connectButton');
+  const mintButton = document.querySelector("#mint-btn");
+
+
 
   //Created check function to see if the MetaMask extension is installed
   const isMetaMaskInstalled = () => {
@@ -43,27 +56,124 @@ const initialize = () => {
     return Boolean(ethereum && ethereum.isMetaMask);
   };
 
-//We create a new MetaMask onboarding object to use in our app
-const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
 
-//This will start the onboarding proccess
-const onClickInstall = () => {
-  onboardButton.innerText = 'Onboarding in progress';
-  onboardButton.disabled = true;
-  //On this object we have startOnboarding which will start the onboarding process for our end user
-  onboarding.startOnboarding();
-};
+//const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+Moralis.start({ serverUrl, appId });
 
-const onClickConnect = async () => {
-  try {
-    // Will open the MetaMask UI
-    // You should disable this button while the request is pending!
-    await ethereum.request({ method: 'eth_requestAccounts' });
-  } catch (error) {
-    console.error(error);
+let user = Moralis.User.current();
+/* Authentication code */
+async function login() {
+
+  if (!user) {
+    user = await Moralis.authenticate({
+      signingMessage: "Log in using Moralis",
+    })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+        onboardButton.innerText = "Connected";
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  } else {
+  console.log("already login");
   }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+  onboardButton.innerText = "Connect";
+}
+
+async function upload(){
+const mySvg = $("#__turtlegraph__").html();
+const base64Data = window.btoa(mySvg);
+const base64Image = `data:image/svg+xml;base64,${btoa(mySvg)}`;
+currentDate = Date.now();
+console.log(currentDate);
+const treeAddress = document.getElementById("treeAddress").value.slice(0,5);
+  console.log(treeAddress);
+//const SvgImage = new Moralis.File("image.svg", {base64: base64Image});
+//await SvgImage.saveIPFS();
+//const imgURL = await SvgImage.ipfs();
+//const metadata = {
+//  "name": WalletTree, //needs to be auto updated
+//  "description": "Unique generative trees built out of ERC721 tokens in your wallet.",
+//  "image": imgURL, // received from Moralis IPFS function.
+//  "edition": 1, //needs to be auto updated
+//  "Block": //block number at creation,
+//  "attributes": [
+//    {
+//      "trait_type": "Leaf Palette",
+//      "value": ""}, //To be caught from leaf_palette_name variable in nft_art.py
+//      {"trait_type": "Root Palette",
+//      "value": ""}, //To be caught from leaf_palette_name variable in nft_art.py
+//  ]
+//}
+
+const metadata = {
+  "name": `WalletTree ${treeAddress}`, //needs to be auto updated
+  "description": "Unique generative trees built out of ERC721 tokens in your wallet.",
+  "image_data": mySvg, // received from Moralis IPFS function.
+  "Block": 000, //block number at creation,
+  "attributes": [
+    {
+      "trait_type": "Leaf Palette",
+      "value": ""}, //To be caught from leaf_palette_name variable in nft_art.py
+      {"trait_type": "Root Palette",
+      "value": ""}, //To be caught from leaf_palette_name variable in nft_art.py
+      {
+      "display_type": "date",
+      "trait_type": "created",
+      "value": currentDate,
+    }
+  ]
+}
+const metadataFile = new Moralis.File("metadata.json", {
+  base64: btoa(JSON.stringify(metadata)),
+});
+await metadataFile.saveIPFS();
+const metadataURL = await metadataFile.ipfs();
+//const contract = new web3.eth.Contract(contractAbi, contractAddress)
+//contract.methods.mint(metadataURL)
+await Moralis.enableWeb3();
+const sendOptions = {
+  contractAddress: contractAddress,
+  functionName: "mint",
+  abi: contractAbi,
+  msgValue: Moralis.Units.ETH("0.03"),
+  params: {
+    uri: metadataURL,
+  },
 };
 
+const transaction = await Moralis.executeFunction(sendOptions);
+await transaction.wait()
+alert(`Minted Successfully! Txn Hash: ${transaction.hash}`)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function onMintClick() {
+
+    console.log(mySvg);
+}
+mintButton.onclick = upload;
+onboardButton.onclick = login;
+
+console.log("useeerrrr"+user);
 
   //------Inserted Code------\\
 const MetaMaskClientCheck = () => {
@@ -78,13 +188,15 @@ const MetaMaskClientCheck = () => {
   } else {
     //If MetaMask is installed we ask the user to connect to their wallet
     onboardButton.innerText = 'Connect';
-    //When the button is clicked we call this function to connect the users MetaMask Wallet
-    onboardButton.onclick = onClickConnect;
-    //The button is now disabled
-    onboardButton.disabled = false;
+//    //When the button is clicked we call this function to connect the users MetaMask Wallet
+//    onboardButton.onclick = await onClickConnect;
+//    //The button is now disabled
+//    onboardButton.disabled = false;
+
   }
 };
 MetaMaskClientCheck();
+login();
 //------/Inserted Code------\\
 };
 
